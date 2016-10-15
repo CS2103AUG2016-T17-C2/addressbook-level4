ec6c456c373e2b5f6c700031c077128b9f2e1223
hotchocolatemarshmallow <hotchocolatemarshmallow35211@gmail.com>
2016-10-15 22:02:19 +0800

    untested file changes to shortcut

---
+++ src/main/java/seedu/task/commons/util/ShortcutUtil.java
@@ -0,0 +1,72 @@
+package seedu.task.commons.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import seedu.task.commons.core.ShortcutSetting;
+import seedu.task.commons.core.LogsCenter;
+import seedu.task.commons.exceptions.DataConversionException;
+
+/**
+ * A class for accessing the shortcut File.
+ */
+
+public class ShortcutUtil {
+
+    private static final Logger logger = LogsCenter.getLogger(ShortcutUtil.class);
+
+    /**
+     * Returns the Shortcut object from the given file or
+     * {@code Optional.empty()} object if the file is not found. If any values
+     * are missing from the file, default values will be used, as long as the
+     * file is a valid json file.
+     * 
+     * @param shortcutFilePath
+     *            cannot be null.
+     * @throws DataConversionException
+     *             if the file format is not as expected.
+     */
+    public static Optional<ShortcutSetting> readShortcut(String shortcutFilePath) throws DataConversionException {
+
+        assert shortcutFilePath != null;
+
+        File shortcutFile = new File(shortcutFilePath);
+
+        if (!shortcutFile.exists()) {
+            logger.info("shortcut file " + shortcutFile + " not found");
+            return Optional.empty();
+        }
+
+        ShortcutSetting shortcut;
+
+        try {
+            shortcut = FileUtil.deserializeObjectFromJsonFile(shortcutFile, ShortcutSetting.class);
+        } catch (IOException e) {
+            logger.warning("Error reading from shortcut file " + shortcutFile + ": " + e);
+            throw new DataConversionException(e);
+        }
+
+        return Optional.of(shortcut);
+    }
+
+    /**
+     * Saves the Shortcut object to the specified file. Overwrites existing file
+     * if it exists, creates a new file if it doesn't.
+     * 
+     * @param shortcut
+     *            cannot be null
+     * @param shortcutFilePath
+     *            cannot be null
+     * @throws IOException
+     *             if there was an error during writing to the file
+     */
+    public static void saveShortcut(ShortcutSetting shortcut, String shortcutFilePath) throws IOException {
+        assert shortcut != null;
+        assert shortcutFilePath != null;
+
+        FileUtil.serializeObjectToJsonFile(new File(shortcutFilePath), shortcut);
+    }
+
+}
---
+++ src/main/java/seedu/task/logic/commands/ShortcutCommand.java
@@ -0,0 +1,92 @@
+package seedu.task.logic.commands;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.logging.Logger;
+import seedu.task.commons.core.LogsCenter;
+import seedu.task.commons.core.ShortcutSetting;
+import seedu.task.commons.exceptions.DataConversionException;
+import seedu.task.commons.util.ShortcutUtil;
+import seedu.task.commons.util.StringUtil;
+
+/**
+ * Changes the Command names of taskBook.
+ */
+
+public class ShortcutCommand extends Command {
+
+    public static final String COMMAND_WORD = "changeShortcut";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Changes the command name of commands. " + "Example: "
+            + COMMAND_WORD;
+    // TODO: figure out how to type example
+
+    public static final String MESSAGE_SUCCESS = "File path changed";
+    public static final String MESSAGE_DUPLICATE_FILENAME = "This file already exists in the taskBook";
+
+    String shortcutField;
+    String shortcutWord;
+    Logger logger = LogsCenter.getLogger(ShortcutCommand.class);
+
+    /**
+     * Parameter: File Path Object
+     * 
+     * @throws IOException
+     */
+
+    /*
+     * Changes the name of the command
+     */
+    public ShortcutCommand(String shortcutField, String shortcutWord) {
+
+        this.shortcutField = shortcutField;
+        this.shortcutWord = shortcutWord;
+        run();
+    }
+
+    private void run() {
+        ShortcutSetting shortcutSetting;
+        String shortcutFilePathUsed;
+        shortcutFilePathUsed = ShortcutSetting.DEFAULT_SHORTCUT_FILEPATH;
+
+        try {
+            Optional<ShortcutSetting> shortcutOptional = ShortcutUtil.readShortcut(shortcutFilePathUsed);
+            shortcutSetting = shortcutOptional.orElse(new ShortcutSetting());
+        } catch (DataConversionException e) {
+            shortcutSetting = new ShortcutSetting();
+        }
+
+        // Update shortcut file in case it was missing to begin with or there
+        // are
+        // new/unused fields
+        try {
+            switch (this.shortcutField) {
+
+            case AddCommand.COMMAND_WORD:
+                shortcutSetting.setAdd(this.shortcutWord);
+
+            case DeleteCommand.COMMAND_WORD:
+                shortcutSetting.setDelete(this.shortcutWord);
+
+            case ListCommand.COMMAND_WORD:
+                shortcutSetting.setList(this.shortcutWord);
+
+            default:
+                // tell user that field does not exist
+            }
+
+            ShortcutUtil.saveShortcut(shortcutSetting, shortcutFilePathUsed);
+        } catch (IOException e) {
+            logger.warning("Failed to save shortcut change : " + StringUtil.getDetails(e));
+        }
+
+    }
+
+    @Override
+    public CommandResult execute() {
+        assert model != null;
+        return new CommandResult(String.format(MESSAGE_SUCCESS, shortcutWord));
+
+    }
+
+}
