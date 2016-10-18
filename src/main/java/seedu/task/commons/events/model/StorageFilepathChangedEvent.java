90c6392417f36c1c6b15656cb1822cbb67a65a42
hotchocolatemarshmallow <hotchocolatemarshmallow35211@gmail.com>
2016-10-18 15:04:39 +0800

    Forgot to commit this file

---
+++ src/main/java/seedu/task/commons/events/model/StorageFilepathChangedEvent.java
@@ -0,0 +1,29 @@
+package seedu.task.commons.events.model;
+
+import seedu.task.commons.core.Config;
+import seedu.task.commons.events.BaseEvent;
+
+/** Indicates the Filepath in the Config has changed*/
+
+public class StorageFilepathChangedEvent extends BaseEvent{
+    public final String newConfigFilepathString;
+    Config config;
+    
+    
+    public StorageFilepathChangedEvent( Config initializedConfig){
+        this.config = initializedConfig;
+        this.newConfigFilepathString = Config.DEFAULT_CONFIG_FILE;
+    }
+    
+   
+   
+    @Override
+    public String toString() {
+        return "File Path changed to" + config.getTaskBookFilePath();
+   
+    }
+
+
+
+
+}
