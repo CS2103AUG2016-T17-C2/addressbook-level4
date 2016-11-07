31b36c9f3a4da91d71954ba62c83f78747c9051a
Abdul Rahman <arsh.abdulrahman@hotmail.com>
2016-11-07 14:10:59 +0800

    clean up

--- src/test/java/guitests/AddCommandTest.java
+++ src/test/java/guitests/AddCommandTest.java
@@ -22,12 +22,6 @@ public class AddCommandTest extends TaskBookGuiTest {
         TestTask taskToAdd = TypicalTestTasks.play;
         TestTask[] currentList = td.getTypicalTasks();
         currentList = TestUtil.addTasksToList(currentList, taskToAdd);
-        //assertAddSuccess(taskToAdd, currentList); 
-
-        //add task with dates clashing with other tasks
-        taskToAdd = TypicalTestTasks.coding;
-        commandBox.runCommand(taskToAdd.getAddCommand());
-        //assertResultMessage(String.format(Task.DATE_CLASH_MESSAGE, "code cs2103", "Fri Nov 04 17:50:00 SGT 2016", "Fri Nov 04 17:50:00 SGT 2016"));
 
         //add to empty list
         commandBox.runCommand("clear");
